c54aa775358195401eabc58fd43a2acb8421b775
itsmesroy <itsmevicky78@gmail.com>
2024-10-24 21:31:52 +0530

    making few changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import MediaCard from './cards.jsx'
-import datas from './data.json'
+//import datas from './data.json'
 // import { Card } from '@mui/material';
- import { useState } from 'react';
+ import { useEffect, useState } from 'react';
+ import Grid from '@mui/material/Grid2';
+ import Box from '@mui/material/Box';
 
 function App() {
-console.log(datas);
-const [videosData, setvideosData]=useState(datas.videos);
 
-  return <div className="App"> {videosData.map((video) =>{
-      return (
-        
-        <MediaCard />
-        
-      );      
-    })}</div>;
+const [videosData, setvideosData]=useState([]);
+
+const fetchVideosData= async()=>{
+  try{
+    const res = await fetch("https://content-xflix-backend.azurewebsites.net/v1/videos ")
+    const data= await res.json();
+    setvideosData(data.videos);
+  }catch(err){
+    console.error(err);
+    alert("Error fetching videos. Please try again later.")  // Add a more robust error handling mechanism here.
+  }
+};
+useEffect(()=>{
+  fetchVideosData();
+},[]);
+
+  return (
+      
+          <Grid container > 
+          {videosData.map((video, Item) =>{         
+            <Grid key={video.id} size={{xs:12,   md:6, lg:3}} >
+            <MediaCard videos={video} />
+          </Grid>   
+          })}       
+        </Grid>
      
-}
+        )
+      }
 
 export default App;
